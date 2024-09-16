f930ed664f012812a751a5c909413b7a2197e613
pawan <pawan220787@gmail.com>
2024-09-16 14:33:12 +0530

    Added new automation project

--- src/Hompage/FlightSearch.java
+++ src/Hompage/FlightSearch.java
@@ -1,4 +1,21 @@
 package Hompage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
+
 public class FlightSearch {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.edge.driver", "C:\\Windows\\System32\\msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.alaskaair.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("oneWay")).click();
+        driver.findElement(By.id("fromCity1")).sendKeys("london");
+        driver.findElement(By.xpath("//li[@title='London, United Kingdom (LCY-London City)']")).click();
+
+    }
 }
